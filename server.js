d8494c67ec69471ddb2d39070fa29ad196cfebe0
Jimly23 <jimlyasidqi692@gmail.com>
2025-05-21 20:50:55 +0700

    First Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/home', (req, res) => {
+  res.send('Hello Home!');
+});
+
+app.get('/admin', (req, res) => {
+  res.send('Hello Admin!');
+});
+
+app.get('/profile', (req, res) => {
+  res.send('Hello Profile!');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Hello Contact!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('Hello About!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
